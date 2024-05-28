3518670496cbbbe6867875989b1d418da88dbc26
Rin4430 <166579664+Rin4430@users.noreply.github.com>
2024-05-28 12:55:29 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -24,10 +24,10 @@ def cond_prob(table,evidence,evidence_value,target,target_value):
   p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
   return p_b_a
 def prior_prob(table, target, target_value):
-   t_list = up_get_column(table, target)
-   p_a = sum([1 if v==target_value else 0 for v in t_list])/len(t_list)
+  t_list = up_get_column(table, target)
+  p_a = sum([1 if v==target_value else 0 for v in t_list])/len(t_list)
   return p_a
  def compute_probs(neg,pos):
-   p0 = neg/(neg+pos)
-   p1 = pos/(neg+pos)
-   return [p0,p1]
+  p0 = neg/(neg+pos)
+  p1 = pos/(neg+pos)
+  return [p0,p1]
